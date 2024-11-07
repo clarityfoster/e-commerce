db77d86b66af3c1c98b8ad2524e8c1ccb912080c
clarity552 <cherrymyintmo552@gmail.com>
2024-11-07 18:06:01 +0630

    add best seller section

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -9,9 +9,8 @@
         </div>
         <div class="carousel-inner">
             <div class="carousel-item active">
-
                 <div class="home-slider" style="background-image: url({{ asset('img/home/home2.webp') }})">
-                    <div class="custom-container">
+                    <div class="home-container">
                         <div class="home-item">
                             <img src="{{ asset('img/home/home-item2.webp') }}" alt="" class="home-item-img">
                             <span class="item-name">Oversized Cotton Hoodie</span>
@@ -45,7 +44,68 @@
             <span class="visually-hidden">Next</span>
         </button>
     </div>
-    <div class="second" style="height: 100vh">
-
+    <div class="bestSeller-section custom-container" id="bestSellers">
+        <h1 class="brand-text">Best Sellers</h1>
+        <div class="best-seller-content">
+            <div class="best-seller-items">
+                <div class="best-seller-card">
+                    <img src="{{ asset('img/bestSeller/sweater1.avif') }}" alt="" class="best-seller-img">
+                    <h3 class="best-seller-card-title">
+                        Polyester long coat
+                    </h3>
+                    <h3 class="best-seller-card-price">
+                        30,000 Kyats
+                    </h3>
+                </div>
+                <div class="best-seller-card">
+                    <img src="{{ asset('img/bestSeller/sweater2.avif') }}" alt="" class="best-seller-img">
+                    <h3 class="best-seller-card-title">
+                        Polyester long coat
+                    </h3>
+                    <h3 class="best-seller-card-price">
+                        30,000 Kyats
+                    </h3>
+                </div>
+                <div class="best-seller-card">
+                    <img src="{{ asset('img/bestSeller/sweater3.avif') }}" alt="" class="best-seller-img">
+                    <h3 class="best-seller-card-title">
+                        Polyester long coat
+                    </h3>
+                    <h3 class="best-seller-card-price">
+                        30,000 Kyats
+                    </h3>
+                </div>
+                <div class="best-seller-card">
+                    <img src="{{ asset('img/bestSeller/sweater4.avif') }}" alt="" class="best-seller-img">
+                    <h3 class="best-seller-card-title">
+                        Polyester long coat
+                    </h3>
+                    <h3 class="best-seller-card-price">
+                        30,000 Kyats
+                    </h3>
+                </div>
+                <div class="best-seller-card">
+                    <img src="{{ asset('img/bestSeller/sweater5.avif') }}" alt="" class="best-seller-img">
+                    <h3 class="best-seller-card-title">
+                        Polyester long coat
+                    </h3>
+                    <h3 class="best-seller-card-price">
+                        30,000 Kyats
+                    </h3>
+                </div>
+            </div>
+            <div class="two-categories">
+                <div class="sport-poster" style="background-image: url({{ asset('img/bestSeller/sport-poster.jpg') }})">
+                    <h4 class="sport-poster-title1">Find your favourite</h4>
+                    <h1 class="sport-poster-title2"> <span class="different-colors">S</span>port<span class="different-colors">wears</span> </h1>
+                    <a href="" class="shop-now">Shop Now</a>
+                </div>
+                <div class="sport-poster" style="background-image: url({{ asset('img/bestSeller/streetstyle-poster.webp') }})">
+                    <h4 class="sport-poster-title1">Find your favourite</h4>
+                    <h1 class="sport-poster-title2">St<span class="different-colors">r</span>e<span class="different-colors">e</span>t<span class="different-colors">St</span>yle </h1>
+                    <a href="" class="shop-now">Shop Now</a>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
